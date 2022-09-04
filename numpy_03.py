30b3279f0721ac0c1efb38f8e35f2b067554faf2
seokjun jin <bbjoony@gmail.com>
2022-09-04 21:52:47 +0900

    check type, transpose added

--- numpy_03.py
+++ numpy_03.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 a = ([1,2,3,4,5], [2,3,4,5,6])
+len_a = len(a)
+# type_a = a.type()
+
 print(a)
+print(len_a)
+print(type(a))
+
+b = np.zeros((2,10))
+c = np.ones((2,10))
+d = np.arange(2,10)
+e = np.transpose(a)
+
+print(d)
+print(e)
