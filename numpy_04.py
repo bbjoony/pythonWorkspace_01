b74aed51fcd81c4b4fdee64d48162fe1ceaa1e8b
seokjun jin <bbjoony@gmail.com>
2022-09-08 22:34:54 +0900

    array operation handle

---
+++ numpy_04.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr1 = np.array([[2,3,4], [6,7,8]])
+arr2 = np.array([[12,23,14], [46,13,11]])
+arr3 = np.array([100,200,300]) #It has a different size with upper arraies 
+
+shape_a = arr1.shape
+shape_c = arr3.shape
+
+# print(arr1)
+# print(arr2)
+# print(arr1/arr2)
+
+print(shape_a)
+print(shape_c)
