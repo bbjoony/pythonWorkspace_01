a75605f6cba043303b3de5d68e1d844dc79a1add
seokjun jin <bbjoony@gmail.com>
2022-09-08 21:59:57 +0900

    len added and printed

--- numpy_03.py
+++ numpy_03.py
@@ -9,9 +9,14 @@ print(len_a)
 print(type(a))
 
 b = np.zeros((2,10))
-c = np.ones((2,10))
+c = np.ones((1,10))
 d = np.arange(2,10)
 e = np.transpose(a)
 
-print(d)
-print(e)
+len_c = len(c) #열값만 출력함(몇 개의 열이 있는지로 length를 측정)
+len_e = len(e)
+
+print("b=", b)
+print("c=", c, len_c)
+print("d=", d)
+print("e=", e, len_e)
