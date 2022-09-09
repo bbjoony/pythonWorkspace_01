601ace115f81f56e93104eaee5d4c576bd5de242
seokjun jin <bbjoony@gmail.com>
2022-09-09 21:05:01 +0900

    Pandas로 코드 치환

--- numpy_07.py
+++ numpy_07.py
@@ -2,16 +2,11 @@ import numpy as np
 import pandas as pd 
 import os, re
 
-#os.chdir(r '../Users/devsiters/Documents/GitHub/pythonWorkspace_01')
-#quest = np.array(usecsv.switch(usecsv.opencsv('quest.csv')))
-
-# with open("quest.csv") as file_name:
-#     array = np.loadtxt(file_name, delimiter=",")
-
 csv_test = pd.read_csv('quest.csv')
-print(csv_test)
+df = pd.DataFrame(csv_test)
 
-csv_test_01 = csv_test > 5
-type_d = type(csv_test_01)
-print(csv_test_01)
-print(type_d)
+
+df = df.replace(6,5)
+print(df)
+
+df.to_csv('quest_01.csv')
