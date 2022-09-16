52d8977c78fdb2c44a306007ad0b37bf76f6124d
seokjun jin <bbjoony@gmail.com>
2022-09-16 15:41:33 +0900

    주석 첨삭

--- random_lottery.py
+++ random_lottery.py
@@ -1,10 +1,11 @@
 import random
 
-my_list = []
+my_list = [] 
 
 for i in range(6):
-    a = random.randint(1,46)
-    while a in my_list:
+    a = random.randint(1,46) #실제 배열 안에 기록될 랜덤한 숫자 'a'
+    
+    while a in my_list: 
         a = random.randint(1, 46)
     my_list.append(a)
        
