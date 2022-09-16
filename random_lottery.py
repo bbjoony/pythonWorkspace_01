0f0d67119f8a0af379691ad3fc60a67b2a6e8078
seokjun jin <bbjoony@gmail.com>
2022-09-16 14:47:03 +0900

    랜덤으로 중복되지 않은 숫자 6개를 추출해 배열에 저장함

--- random_lottery.py
+++ random_lottery.py
@@ -1,14 +1,11 @@
 import random
 
 my_list = []
-maxvalue = 0
-maxelement = 0
 
-for i in range(100):
-    my_list.append(random.randrange(0,46))
-
-for i in set(my_list):
-    if my_list.count(i) > maxvalue:
-        maxelement, maxvalue = i, my_list.count(i)
-        
-print('제일 많이 나온 숫자는 %d, 등장 횟수는 %d 입니다.' % (maxelement, maxvalue))
+for i in range(6):
+    a = random.randint(1,46)
+    while a in my_list:
+        a = random.randint(1, 46)
+    my_list.append(a)
+       
+print(my_list)
